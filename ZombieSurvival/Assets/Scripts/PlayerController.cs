d9a0b64a0ad3cd5662756482b11453a85dcc750b
Michael Jeppesen <mj@lpj-software.dk>
2021-05-02 16:38:45 +0200

    unity merge

--- ZombieSurvival/Assets/Scripts/PlayerController.cs
+++ ZombieSurvival/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10f;
-    public float speedCap = 20f;
-    public float magnitude;
-    private Rigidbody rigidbody = null;
+    public float speed = 2f;
+    public float jumpVel = 4f;
+    public float onGroundDist = 1.01f;
+    public float sensitivity = 1f;
+    private new Rigidbody rigidbody;
+    public Camera camera = null;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,25 +19,46 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        int f = 0, r = 0;
+        float f = 0, r = 0;
         if (Input.GetKey(KeyCode.W))
             f++;
         if (Input.GetKey(KeyCode.S))
             f--;
         if (Input.GetKey(KeyCode.A))
-            r++;
-        if (Input.GetKey(KeyCode.D))
             r--;
+        if (Input.GetKey(KeyCode.D))
+            r++;
 
-        Vector3 v = new Vector3(f, 0, r);
-
-        rigidbody.AddForce(v.normalized * speed * Time.deltaTime, ForceMode.Acceleration);
-        if (rigidbody.velocity.magnitude > speedCap)
-        {
-            rigidbody.velocity *= speedCap / rigidbody.velocity.magnitude;
+        if (Input.GetKey(KeyCode.Space)) {
+            if (Physics.Raycast(transform.position, Vector3.down, onGroundDist))
+                rigidbody.velocity = new Vector3(0, jumpVel, 0);
         }
 
-        magnitude = rigidbody.velocity.magnitude;
-//        trans.position += v.normalized * Time.deltaTime;
+        Vector3 v = new Vector3(r, 0, f);
+        v = Quaternion.AngleAxis(camera.transform.rotation.eulerAngles.y, Vector3.up) * v;
+        rigidbody.position = Vector3.MoveTowards(rigidbody.position, v.normalized + rigidbody.position, Time.fixedDeltaTime * speed);
+
+        f = 0; 
+        r = 0;
+        if (Input.GetKey(KeyCode.UpArrow))
+            f++;
+        if (Input.GetKey(KeyCode.DownArrow))
+            f--;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            r--;
+        if (Input.GetKey(KeyCode.RightArrow))
+            r++;
+
+        r += sensitivity * Input.GetAxis("Mouse X");
+        f += sensitivity * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = transform.eulerAngles + new Vector3(0f, 90f * r * Time.fixedDeltaTime, 0f);
+        Vector3 udDir = camera.transform.eulerAngles + new Vector3(-90f * f * Time.fixedDeltaTime, 0f ,0f);
+        float d = udDir.x;
+        if (d > 180)
+            d = Mathf.Clamp(d, 270f, 360f);
+        else
+            d = Mathf.Clamp(d, -1f, 90f);
+        camera.transform.eulerAngles = new Vector3(d, udDir.y, udDir.z);
     }
 }
