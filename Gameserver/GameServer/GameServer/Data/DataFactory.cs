c93b110b736150f83b5fb1dd372ae6f11ee38c70
Michael Jeppesen <mj@lpj-software.dk>
2021-05-03 17:07:25 +0200

    added data classes to transfere data across connections

---
+++ Gameserver/GameServer/GameServer/Data/DataFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameServer.Data
+{
+    class DataFactory
+    {
+        public IData BytesToData(byte[] bytes)
+        {
+            return null;
+        }
+    }
+}
---
+++ Gameserver/GameServer/GameServer/Data/Position.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameServer.Data
+{
+    class Position : IData
+    {
+        public static readonly byte Signature = 0b11111111;
+
+        private float x, y, z;
+
+        public Position(byte[] bytes)
+        {
+            x = BitConverter.ToSingle(bytes, 1);
+            y = BitConverter.ToSingle(bytes, 5);
+            z = BitConverter.ToSingle(bytes, 9);
+        }
+
+        public int SizeOf() => 13;
+
+        public byte[] toBytes()
+        {
+            byte[] bytes = new byte[13];
+            bytes[0] = Signature;
+            BitConverter.GetBytes(x).CopyTo(bytes, 1);
+            BitConverter.GetBytes(y).CopyTo(bytes, 5);
+            BitConverter.GetBytes(z).CopyTo(bytes, 9);
+            return bytes;
+        }
+    }
+}
