623096f19c9a78b21774f68429c1bab9d3b5168a
Michael Jeppesen <mj@lpj-software.dk>
2021-05-05 15:29:59 +0200

    Added a shortcut to run scene 0

---
+++ ZombieSurvival/Assets/Editor/Play scene 0.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Playscene0 : MonoBehaviour
+{
+    [MenuItem("Edit/Play-Stop, But From Prelaunch Scene %0")]
+    public static void PlayFromPrelaunchScene()
+    {
+        if (EditorApplication.isPlaying == true)
+        {
+            EditorApplication.isPlaying = false;
+            return;
+        }
+
+        EditorApplication.SaveCurrentSceneIfUserWantsTo();
+        EditorApplication.OpenScene("Assets/Scenes/Main.unity");
+        EditorApplication.isPlaying = true;
+    }
+}
